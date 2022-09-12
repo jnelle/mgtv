d92afd947e82d519ef171c7d724fb9ad2ec43ea8
jnelle <jimmy@nelle.sh>
2022-09-12 20:34:02 +0200

    refactor: move auth repository in specific dir
    Signed-off-by: jnelle <jimmy@nelle.sh>

--- lib/data/repository/auth/auth_repository_impl.dart
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mgtv/data/models/result/result.dart';
 import 'package:mgtv/data/remote/auth/auth_data.dart';
-import 'package:mgtv/data/repository/auth_repository.dart';
+import 'package:mgtv/data/repository/auth/auth_repository.dart';
 import 'package:retrofit/retrofit.dart';
 
 final Provider<AuthRepositoryImpl> authRepositoryProvider =
