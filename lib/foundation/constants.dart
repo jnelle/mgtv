2e03c124a49119f70b69df403525a0e3ba892bcd
jnelle <jimmy@nelle.sh>
2022-09-12 20:35:45 +0200

    feat(config): extend config
    Signed-off-by: jnelle <jimmy@nelle.sh>

--- lib/foundation/constants.dart
+++ lib/foundation/constants.dart
@@ -1,13 +1,22 @@
 class Constants {
   Constants({
-    required this.endpoint,
+    required this.session,
+    required this.httpBaseUri,
+    required this.email,
+    required this.password,
   });
 
   factory Constants.of() {
     return Constants(
-      endpoint: 'https://massengeschmack.tv',
+      httpBaseUri: 'https://massengeschmack.tv',
+      session: 'mgtv_session',
+      email: 'email',
+      password: 'password',
     );
   }
 
-  final String endpoint;
+  final String httpBaseUri;
+  final String session;
+  final String email;
+  final String password;
 }
