7c9ce17ef31e16c65d84703a15b13460c9716863
jnelle <jimmy@nelle.sh>
2022-09-12 20:30:29 +0200

    feat(storage): add flutter secure storage
    Signed-off-by: jnelle <jimmy@nelle.sh>

---
+++ lib/data/provider/storage_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final Provider<FlutterSecureStorage> storageProvider =
+    Provider<FlutterSecureStorage>(((_) => const FlutterSecureStorage()));
