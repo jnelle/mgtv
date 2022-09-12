257859274a1e49332893af89fb860ab981eaa0d4
jnelle <jimmy@nelle.sh>
2022-09-12 20:22:12 +0200

    feat(colors): generate color class with flutter gen
    Signed-off-by: jnelle <jimmy@nelle.sh>

--- lib/gen/colors.gen.dart
+++ lib/gen/colors.gen.dart
@@ -60,6 +60,33 @@ class ColorName {
   ///   700: #FFED7E00
   ///   800: #FFEB7400
   ///   900: #FFE76200
+  static const MaterialColor primaryColor = MaterialColor(
+    0xFFF19100,
+    <int, Color>{
+      50: Color(0xFFFDF2E0),
+      100: Color(0xFFFBDEB3),
+      200: Color(0xFFF8C880),
+      300: Color(0xFFF5B24D),
+      400: Color(0xFFF3A226),
+      500: Color(0xFFF19100),
+      600: Color(0xFFEF8900),
+      700: Color(0xFFED7E00),
+      800: Color(0xFFEB7400),
+      900: Color(0xFFE76200),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFFDF2E0
+  ///   100: #FFFBDEB3
+  ///   200: #FFF8C880
+  ///   300: #FFF5B24D
+  ///   400: #FFF3A226
+  ///   500: #FFF19100
+  ///   600: #FFEF8900
+  ///   700: #FFED7E00
+  ///   800: #FFEB7400
+  ///   900: #FFE76200
   static const MaterialColor submitButtonBackground = MaterialColor(
     0xFFF19100,
     <int, Color>{
